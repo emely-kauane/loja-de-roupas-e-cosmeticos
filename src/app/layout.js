3ad182684897877aab35c93966fa10fc1db5cae0
Emely Kauane <emely.kauane@academico.ifpb.edu.br>
2025-04-15 10:06:18 -0300

    definição de uma rota e do cabeçalho padrão

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <header>
+          <center>
+            <h1>cabeçalho</h1>
+          </center>
+        </header>
         {children}
       </body>
     </html>
